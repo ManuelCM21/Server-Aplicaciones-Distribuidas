009e08fac625dec35a4b1b87b4f08188524c6bd9
ManuelCM21 <manuelchuncamamani@gmail.com>
2023-04-24 12:41:33 -0500

    Servidor

--- config-serve/src/test/java/com/example/configserve/ConfigServeApplicationTests.java
+++ config-serve/src/test/java/com/example/configserve/ConfigServeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.configserver;
+package com.example.configserve;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ConfigServeApplicationTests {
 
 	@Test
 	void contextLoads() {
